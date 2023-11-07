c5962635b1917dbf1bca4c0a3b6a4252e7bb321c
nareshrao <nareshraoss@gmail.com>
2023-11-07 17:59:48 +0530

    first commit

---
+++ Phase 2/Module 1/Practice Project 1/User Login/src/main/java/login.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        PrintWriter pw = response.getWriter();
+
+        if (username.equals("naresh") && password.equals("naresh")) {
+            pw.println("<h2>Login Successful!</h2>");
+        } else {
+            pw.println("<h2>Invalid credentials. Please try again.</h2>");
+        }
+	}
+
+}
